c67f0e7904c98f98fe45f183f6263994784482c6
Tariroyashe <rukodzac@gmail.com>
2023-10-04 15:27:38 -0700

    optional tasks

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# Define a function called remove_char_at that takes two parameters:
+# - str: the input string
+# - n: the position of the character to remove
+def remove_char_at(str, n):
+    
+    # Check if the given position (n) is negative
+    if n < 0:
+        # If n is negative, return the original string unchanged
+        return (str)
+    
+    # If n is non-negative (greater than or equal to 0), create a
+    # new strngby concatenating two slices of the input string:
+    # - str[:n] rep the part of the string before the char at position n
+    # - str[n+1:] rep the part of the strng after the char at position n
+    # This effectively removes the character at position n.
+    return (str[:n] + str[n+1:])
---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+
+    # Check if 'a' is less than 'b'
+    if a < b:
+        # If 'a' is less than 'b', return the value of 'c'
+        return (c)
+    
+    # Check if 'c' is greater than 'b'
+    if c > b:
+        # If 'c' is greater than 'b', return the sum of 'a' and 'b'
+        return (a + b)
+
+    # If none of the above conditions are met, return the result
+    # of 'a' multiplied by 'b' minus 'c'
+    return (a * b - c)
