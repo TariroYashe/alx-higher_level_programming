d90a825f0ace9965b791117c261c3ab27b95c79e
Tariroyashe <rukodzac@gmail.com>
2023-10-05 07:48:32 -0700

    program that imports functions from the file calculator_1.py, does some Maths, and prints the result

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 
-# Import specific functions from the 'calculator_1' module
+# Import functions from calculator_1.py
 from calculator_1 import add, sub, mul, div
 
-# Define variables 'a' and 'b' with values 10 and 5
+# Define variables a and b on separate lines
 a = 10
 b = 5
 
-# Perform the mathematical operations and print the results
+# Call the imported functions using a and b, and print the results
 result_add = add(a, b)
 result_sub = sub(a, b)
 result_mul = mul(a, b)
 result_div = div(a, b)
 
-# Print the results
-print(f"Addition: {a} + {b} = {result_add}")
-print(f"Subtraction: {a} - {b} = {result_sub}")
-print(f"Multiplication: {a} * {b} = {result_mul}")
-print(f"Division: {a} / {b} = {result_div}")
+# Print the results without using print more than 4 times
+print("{} + {} = {}".format(a, b, result_add))
+print("{} - {} = {}".format(a, b, result_sub))
+print("{} * {} = {}".format(a, b, result_mul))
+print("{} / {} = {}".format(a, b, result_div))
