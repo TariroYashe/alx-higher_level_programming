c55a0497c2218f095954c8597a79aab1be3292cd
Tariroyashe <rukodzac@gmail.com>
2023-10-05 08:21:17 -0700

    program that imports functions from the file calculator_1.py, does some Maths, and prints the result

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,17 +3,14 @@
 # Import functions from calculator_1.py
 from calculator_1 import add, sub, mul, div
 
-# Define variables a and b on separate lines
 a = 10
 b = 5
 
-# Call the imported functions using a and b, and print the results
 result_add = add(a, b)
 result_sub = sub(a, b)
 result_mul = mul(a, b)
 result_div = div(a, b)
 
-# Print the results without using print more than 4 times
 print("{} + {} = {}".format(a, b, result_add))
 print("{} - {} = {}".format(a, b, result_sub))
 print("{} * {} = {}".format(a, b, result_mul))
