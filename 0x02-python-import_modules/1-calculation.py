c6a4a0136ebd4570e41dfe52d52e1832c94a3fad
Tariroyashe <rukodzac@gmail.com>
2023-10-10 05:34:30 -0700

    program that imports functions from the file calculator_1.py, does some Maths, and prints the result

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
+# Import the functions from calculator_1.py
+from calculator_1 import add, sub, mul, div
 
-# Check if this script is being run as the main program
-if __name__ == '__main__':
-    # Import functions add, sub, mul, and div from calculator_1 module
-    from calculator_1 import add, sub, mul, div
-    
-    # Define two numbers for calculation
-    a = 10
-    b = 5
-    
-    # Perform addition and print the result
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    
-    # Perform subtraction and print the result
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    
-    # Perform multiplication and print the result
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    
-    # Perform division and print the result
-    print("{} / {} = {}".format(a, b, div(a, b)))
+# Define the variables a and b
+a = 10
+b = 5
+
+# Call each of the imported functions with a and b
+result_add = add(a, b)
+result_sub = sub(a, b)
+result_mul = mul(a, b)
+result_div = div(a, b)
+
+# Print the results, but not more than 4 times as required
+print("Result of addition:", result_add)
+print("Result of subtraction:", result_sub)
+print("Result of multiplication:", result_mul)
+print("Result of division:", result_div)
