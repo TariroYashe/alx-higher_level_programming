722764665f51426464175224415781553b6d2d25
Tariroyashe <rukodzac@gmail.com>
2023-10-10 04:08:47 -0700

    function that deletes the item at a specific position in a list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    # Check if idx is negative or out of range
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-
-    # Initialize a new list to store the result
-    new_list = []
-
-    # Iterate through the original list and skip the item at idx
-    for i in range(len(my_list)):
-        if i != idx:
-            new_list.append(my_list[i])
-
-    return new_list
+    """Delete an item at a specific position in a list."""
+    if 0 <= idx < len(my_list):
+        my_list = my_list[:idx] + my_list[idx+1:]
+    return (my_list)
