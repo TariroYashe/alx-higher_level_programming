98bba80e384703f894475f23fd31a827c9c4f578
Tariroyashe <rukodzac@gmail.com>
2023-10-11 04:41:51 -0700

    Optional task

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    # Create a copy of the original dictionary to avoid modifying it directly
-    new_dict = a_dictionary.copy()
+    list_keys = list(a_dictionary.keys())
 
-    # Iterate over the keys and values in the dictionary
-    for key, val in a_dictionary.items():
-        if val == value:
-            # If the value matches the specified value, del key from the copy
-            del new_dict[key]
+    for value_dic in list_keys:
+        if value == a_dictionary.get(value_dic):
+            del a_dictionary[value_dic]
 
-    return new_dict
+    return (a_dictionary)
