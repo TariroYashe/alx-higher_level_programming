58192ecd12a66a5215d81c7baacafcd4ce2fd8e0
Tariroyashe <rukodzac@gmail.com>
2023-10-11 19:55:52 -0700

    infinit add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
 
-# Check if this script is the main entry point
 if __name__ == "__main__":
-    # Import necessary modules
-    import sys, math
-    
-    # Initialize a variable to store the result
-    result = 0
-    
-    # Iterate through command-line arguments
-    for i in sys.argv:
-        # Convert the argument to an integer and add it to the result
-        result += int(i)
-        
-        # Print the current result
-        print("{}".format(result))
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
