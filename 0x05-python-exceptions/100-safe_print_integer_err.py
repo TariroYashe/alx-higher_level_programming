c989a7a6b5cbcc559e00642700f7db324a3b1d86
Tariroyashe <rukodzac@gmail.com>
2023-10-23 03:09:24 -0700

    python-exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,12 +4,9 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        # Try to format the value as an integer and print it
-        formatted_value = "{:d}".format(value)
-        print(formatted_value)
-        return True
-    except (ValueError, TypeError):
-        # Catch exceptions for non-integer values
-        error_message = "Exception: {} is not an integer".format(value)
-        print(error_message, file=sys.stderr)
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
         return False
+    else:
+        return True
