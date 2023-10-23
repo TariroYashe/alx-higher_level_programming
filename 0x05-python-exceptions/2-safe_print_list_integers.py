05a3ed390d5d9b1661123edb506d98816c18bc49
Tariroyashe <rukodzac@gmail.com>
2023-10-23 03:02:25 -0700

    python-exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(item, int):
                 print("{:d}".format(item), end='')
                 count += 1
-    except (IndexError, ValueError):
+    except IndexError:  # Catch IndexError separately
         pass
 
     print()  # Print a newline after all integers are printed
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a / b
-    except ZeroDivisionError:
-        print("Division by zero is not allowed")
-        result = None
-    except Exception as e:
-        print("An error occurred:", e)
-        result = None
+        div = a / b
+    except (ZeroDivisionError):
+        div = None
     finally:
-        print("Inside result: {}".format(result))
-        return result
+        print("Inside result: {}".format(div))
+        return div
