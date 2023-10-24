2bf22f22ae0ed067aec953e520cf6e9c6e091ec0
Tariroyashe <rukodzac@gmail.com>
2023-10-24 05:27:52 -0700

    python-classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,34 +2,14 @@
 
 class Square:
     """
-    This class defines a square.
+    This is the Square class.
 
-    The Sqre class is used to represent a sqre shape. It can be instantiated
-    to create square objects, and provides methods and attributes for working
-    with squares.
+    The purpose of this class is to define a square.
 
     Attributes:
-        side_length (int): The length of each side of the square.
+        None
 
     Methods:
-        __init__(self, side_length): Initializes a sqre wth the given side len
-        area(self): Computes and returns the area of the square.
+        None
     """
-
-    def __init__(self, side_length=0):
-        """
-        Initializes a square with the given side length.
-
-        Args:
-            side_length (int): The length of each side of the square.
-        """
-        self.side_length = side_length
-
-    def area(self):
-        """
-        Computes and returns the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.side_length ** 2
+    pass
