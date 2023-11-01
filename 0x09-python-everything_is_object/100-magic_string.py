10ff183a6d057b20f96aef0964c66c8606698ac6
Tariroyashe <rukodzac@gmail.com>
2023-11-01 05:46:45 -0700

    0x09-python-everything_is_object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def magic_string():
-    if not hasattr(magic_string, "count"):
-        magic_string.count = 0
-    magic_string.count += 1
-    return "BestSchool" * magic_string.count
 
+def magic_string():
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return "BestSchool" + (", BestSchool" * magic_string.count)
+
+print(*[magic_string() for _ in range(10)], sep='\n')
