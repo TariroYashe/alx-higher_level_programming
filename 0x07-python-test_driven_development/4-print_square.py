6059dbc886441e0dcdeb525ffe593339277efd83
TariroYashe <rukodzac@gmail.com>
2023-11-02 14:56:21 +0200

    python-test_driven_development

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+def print_square(size):
+    """
+    Prints a square with the character '#'.
+
+    Args:
+        size (int): The size of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+
+    Example:
+        >>> print_square(4)
+        ####
+        ####
+        ####
+        ####
+
+        >>> print_square(10)
+        ##########
+        ##########
+        ##########
+        ##########
+        ##########
+        ##########
+        ##########
+        ##########
+        ##########
+        ##########
+
+        >>> print_square(0)
+        (no output)
+
+        >>> print_square(1)
+        #
+
+        >>> print_square(-1)
+        Traceback (most recent call last):
+            ...
+        ValueError: size must be >= 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
+
+if __name__ == "__main__":
+    print_square(4)
+    print("")
+    print_square(10)
+    print("")
+    print_square(0)
+    print("")
+    print_square(1)
+    print("")
+    try:
+        print_square(-1)
+    except Exception as e:
+        print(e)
+    print("")
