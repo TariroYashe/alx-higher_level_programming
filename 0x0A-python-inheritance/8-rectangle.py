492b9c1c373843efc3511d597475b0d268ab26d6
TariroYashe <rukodzac@gmail.com>
2023-11-10 15:44:39 +0200

    Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,43 +1,19 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    """
-    BaseGeometry class containing integer_validator method.
-    """
-    def integer_validator(self, name, value):
-        """
-        Validates if 'value' is a positive integer.
-        Args:
-            name (str): Name of the value being validated.
-            value (int): Value to be validated.
-        Raises:
-            TypeError: If 'value' is not an integer.
-            ValueError: If 'value' is not a positive integer.
-        """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    Rectangle class that inherits from BaseGeometry.
-    """
+    """Represent a rectangle using BaseGeometry."""
+
     def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance with width and height.
+        """Intialize a new Rectangle.
+
         Args:
-            width (int): Width of the rectangle.
-            height (int): Height of the rectangle.
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
         """
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
-
-    def __str__(self):
-        """
-        Returns a string representation of the Rectangle.
-        """
-        return f"[Rectangle] {self.__width}/{self.__height}"
-    
